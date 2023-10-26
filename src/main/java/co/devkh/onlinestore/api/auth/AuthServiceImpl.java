72f0789489a278898e57e3540f8b5489507cbc8e
pisethsattya <pisethsattya33@gmail.com>
2023-10-26 23:20:52 +0700

    verified user use thymleaf (register and verify Generate Code)

--- src/main/java/co/devkh/onlinestore/api/auth/AuthServiceImpl.java
+++ src/main/java/co/devkh/onlinestore/api/auth/AuthServiceImpl.java
@@ -3,18 +3,16 @@ package co.devkh.onlinestore.api.auth;
 import co.devkh.onlinestore.api.auth.web.LoginDto;
 import co.devkh.onlinestore.api.auth.web.RegisterDto;
 import co.devkh.onlinestore.api.auth.web.VerifyGenerateCodeDto;
+import co.devkh.onlinestore.api.mail.Mail;
+import co.devkh.onlinestore.api.mail.MailService;
 import co.devkh.onlinestore.api.user.User;
 import co.devkh.onlinestore.api.user.UserService;
 import co.devkh.onlinestore.api.user.web.NewUserDto;
 import co.devkh.onlinestore.util.RandomUtil;
 import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,7 +23,8 @@ public class AuthServiceImpl implements AuthService{
     private final UserService userService;
     private final AuthRepository authRepository;
     private final AuthMapper authMapper;
-    private final JavaMailSender mailSender;
+    private final MailService mailService;
+
     @Value("${spring.mail.username}")
     private String adminMail;
     @Transactional
@@ -42,20 +41,23 @@ public class AuthServiceImpl implements AuthService{
         authRepository.updateVerifiedCode(registerDto.username(),verifiedCode);
 
         // Send verifiedCode via email
-        MimeMessage mimeMessage = mailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
-        helper.setTo(newUserDto.email());
-        helper.setFrom(adminMail);
-        helper.setSubject("Online Store - Email Verification");
-        helper.setText(String.format("<h1> Your verified code : %s </h1>",verifiedCode),true);
-        mailSender.send(mimeMessage);
+        Mail<String> verifiedMail = new Mail<>();
+        verifiedMail.setSubject("Email Verification");
+        verifiedMail.setSender(adminMail);
+        verifiedMail.setReceiver(newUserDto.email());
+        verifiedMail.setTemplate("auth/verify-mail");
+        verifiedMail.setMetaData(verifiedCode);
+
+        mailService.sendMail(verifiedMail);
+
     }
     @Transactional
     @Override
     public void verifyGenerateCode(VerifyGenerateCodeDto verifyGenerateCodeDto) {
         User verifiedUser = authRepository.findByEmailAndVerifiedCodeAndIsDeletedFalse(verifyGenerateCodeDto.email(),
                 verifyGenerateCodeDto.verifiedCode()).orElseThrow(() ->
-                new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Verify email has been failed..!"));
+                new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                        "Verify email has been failed..!"));
 
         verifiedUser.setIsVerified(true);
         verifiedUser.setVerifiedCode(null);
--- src/main/java/co/devkh/onlinestore/api/auth/web/AuthController.java
+++ src/main/java/co/devkh/onlinestore/api/auth/web/AuthController.java
@@ -4,6 +4,7 @@ import co.devkh.onlinestore.api.auth.AuthService;
 import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,17 +15,21 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+
+    @Value("${app.base-uri}")
+    private String appBaseUri;
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/register")
     public Map<String,String> register(@RequestBody @Valid RegisterDto registerDto) throws MessagingException {
         authService.register(registerDto);
-        return Map.of("message","Please check email and verify..!");
+        return Map.of("message","Please check email and verify..!",
+                "verifyUri",appBaseUri+"auth/verify?email="+registerDto.email());
     }
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/verifyGenerateCode")
     public Map<String,String> verifyGenerateCode(@RequestBody @Valid VerifyGenerateCodeDto verifyGenerateCodeDto){
         authService.verifyGenerateCode(verifyGenerateCodeDto);
-
         return Map.of("message","Congratulation! Email has been verified..!");
     }
 }
